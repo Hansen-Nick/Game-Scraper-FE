47544452b3e397e9338e60c4da4a31802dcabfec
Hansen-Nick <hansen.nick95@gmail.com>
2020-07-09 09:59:13 -0500

    Retrieving data successfully from the database"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [gameData, setGameData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/gamedata")
+      .then((res) => {
+        setGameData(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">WELCOME TO MY GAMESCRAPER</header>
+      <div>{console.log(gameData)}</div>
     </div>
   );
 }
